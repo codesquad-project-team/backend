e8b7c7f99788157d4f310eea9f484285d87de2b6
Min-92 <0201.mj.lee@gmail.com>
2019-10-01 17:38:00 +0900

    feat : sequelize init

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const sequelize = require('./models').sequelize;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+sequelize.sync();
+
 
 const swaggerUi = require('swagger-ui-express');
 

e4167578be3c576eefad5676d36fd763381fa74f
Min-92 <0201.mj.lee@gmail.com>
2019-10-02 21:10:52 +0900

    feat : 테이블간 관계 생성
    - Post : Image = 1 : n
    - Location : Post = 1 : n
    - User : Post = 1 : n
    - User : User = n : m

--- models/index.js
+++ models/index.js
@@ -11,5 +11,19 @@ db.Sequelize = Sequelize;
 
 db.User = require('./user')(sequelize, Sequelize);
 db.Post = require('./post')(sequelize, Sequelize);
+db.Image = require('./image')(sequelize, Sequelize);
+db.Location = require('./location')(sequelize, Sequelize);
+
+db.Post.hasMany(db.Image, {foreignKey : 'post_id' , sourceKey : 'id'});
+db.Image.belongsTo(db.Post, {foreignKey : 'post_id' , targetKey : 'id'});
+
+db.Location.hasMany(db.Post, {foreignKey : 'location_id' , sourceKey : 'id'});
+db.Post.belongsTo(db.Location, {foreignKey : 'location_id' , targetKey : 'id'});
+
+db.User.hasMany(db.Post, {foreignKey : 'user_email' , sourceKey : 'email'});
+db.Post.belongsTo(db.User, {foreignKey : 'user_email' , targetKey : 'email'});
+
+db.User.belongsToMany(db.User, {as : 'followers', foreignKey : 'follower_email', through : 'follow'});
+db.User.belongsToMany(db.User, {as : 'followings', foreignKey : 'follwing_email', through : 'follow'});
 
 module.exports = db;

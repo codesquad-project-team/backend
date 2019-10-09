eef6fd1d5a345576935d1a1937717adf995545de
Min-92 <0201.mj.lee@gmail.com>
2019-10-09 14:47:02 +0900

    fix : config.js 경로 수정

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config.js')[env];
+const config = require(__dirname + '/../../config/config.js')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);

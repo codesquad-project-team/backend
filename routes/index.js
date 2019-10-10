9472cc873b8ff3f99e26117147c93deb7ce75763
Jimin Yeon <gmini.y@gmail.com>
2019-10-10 20:24:47 +0900

    [feat]follow migration add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const model = require('../models');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  const post = await model.Post.findByPk(1)
+  res.send(post);
 });
 
 module.exports = router;

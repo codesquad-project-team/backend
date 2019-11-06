ef111d82a7f04909e8fb632192df56310f1ff05b
Min-92 <0201.mj.lee@gmail.com>
2019-11-07 02:28:38 +0900

    refactor : 회원가입 로직 변경
    - 성공시 이전페이지로 redirect
    - 실패시 임시 토큰 발급, 회원가입페이지로 redirect

--- routes/apiv1/auth.js
+++ routes/apiv1/auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
 
 const router = express.Router();
+
 const expirationDate = '7d';
+const twoMin = 5*60*1000;
 
 module.exports = (models, controller) => {
+    const { User } = models;
 
     router.get('/facebook', passport.authenticate('facebook', {
         scope: ['email']
@@ -36,20 +40,32 @@ module.exports = (models, controller) => {
     router.get('/kakao/callback', (req, res, next) => {
         passport.authenticate('kakao',
             (err, user, info) => {
+                const referer = req.headers.referer || 'http://connectflavor.cf'
+
                 // 로그인 실패
-                // 로그인 페이지로 redirect or front 에 에러 주기
-                if (!user) return res.redirect('/');
+                if (!user) return res.redirect(referer);
 
                 const secret = req.app.get('jwtSecret');
-                const token = jwt.sign({id : user.id, nickname : user.nickname}, secret, {expiresIn : expirationDate});
-                
-                res.cookie('token', token, { domain:'.connectflavor.cf',path: '/', httpOnly: true });
 
-                return res.redirect('/');
+                //nickname 이 없는 경우 => 회원가입
+                if (!user.nickname) {
+                    const tempToken = jwt.sign({ id: user.id, referer }, secret, { expiresIn: '5m' });
+
+                    res.cookie('tempToken', tempToken, { domain: '.connectflavor.cf', path: '/', httpOnly: true, maxAge: twoMin });
+
+                    //redirect : 닉네임 입력 페이지로
+                    return res.redirect('http://connectflavor.cf/signup');
+                }
+
+                //로그인 성공
+                const token = jwt.sign({ id: user.id, nickname: user.nickname }, secret, { expiresIn: expirationDate });
+
+                res.cookie('token', token, { domain: '.connectflavor.cf', path: '/', httpOnly: true });
+
+                return res.redirect(referer);
             }
         )(req, res, next)
-
-
     });
+
     return router
 }

21f1547f956f343ae56a5ac87aa4b3dfd8e8fb54
Min-92 <0201.mj.lee@gmail.com>
2019-11-12 20:25:57 +0900

    feat : POST /validate/nickname 추가
    - 닉네임 검증 api 생성

--- routes/apiv1/validate.js
+++ routes/apiv1/validate.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
+const spaceReg = /\s/g;
 
 module.exports = (models, controller) => {
+    const { User } = models;
+
+    router.post('/nickname', async (req, res, next) => {
+        const { nickname } = req.body;
+
+        // 공백 있는 경우
+        if (spaceReg.test(nickname)) return next(createError(400));
+
+        // 닉네임이 없을 때
+        if (!nickname) return next(createError(400));
+
+        try {
+            const user = await User.findOne({ where: { nickname } });
+
+            // 사용 가능한 닉네임
+            if (!user) return res.sendStatus(200);
+
+            // 중복된 닉네임 
+            return next(createError(409));
+
+        } catch (error) {
+            return next(createError(500));
+        }
+    });
 
     return router;
 }

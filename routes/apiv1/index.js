09ea23cbb97598b97201b0cb948a2a8e7af36188
Min-92 <0201.mj.lee@gmail.com>
2019-11-12 20:25:19 +0900

    feat : validate router 추가

--- routes/apiv1/index.js
+++ routes/apiv1/index.js
@@ -5,11 +5,13 @@ const swaggerSpec = require('../../swagger/apiv1/swagger-definition');
 const userRouter = require('./user');
 const postRouter = require('./post');
 const authRouter = require('./auth');
+const validateRouter = require('./validate');
 
 module.exports = (models, controllers) => {
   router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   router.use('/user', userRouter(models));
   router.use('/post', postRouter(models, controllers.post));
   router.use('/auth', authRouter(models, controllers.auth));
+  router.use('/validate', validateRouter(models, null));
   return router
 }
---
+++ routes/apiv1/validate.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (models, controller) => {
+
+    return router;
+}

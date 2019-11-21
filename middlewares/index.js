22214115fbc3170a3775b99bb7ff3e7318fd77cd
Min-92 <0201.mj.lee@gmail.com>
2019-11-21 17:37:01 +0900

    feat : /auth/has-logged-in api 추가
    - isLoggedIn 미들웨어 생성
    - 로그인 token 정보 변경
    - sawgger 문서 수정

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,15 +1,31 @@
 const jwt = require('jsonwebtoken');
 
-const decodeToken = (req, res, next) => {
+const middlewares = {};
+
+middlewares.decodeToken = (req, res, next) => {
     if (!req.cookies.token) return next();
 
     const secret = req.app.get('jwtSecret');
     try {
-        req.decoded = jwt.verify(req.cookies.token, secret);
+        const decodedToken = jwt.verify(req.cookies.token, secret);
+
+        req.decoded = {
+            id : decodedToken.id,
+            nickname : decodedToken.nickname,
+            profileImage : decodedToken.profileImage
+        } 
+        
     } catch (error) {
         res.clearCookie('token', { path: '/' });
     }
     return next();
 }
 
-module.exports = { decodeToken }
+middlewares.isLoggedIn = (req, res, next) => {
+    if (req.decoded) return next();
+
+    res.status(401);
+    return res.send('Unauthorized');
+}
+
+module.exports = middlewares
--- routes/apiv1/index.js
+++ routes/apiv1/index.js
@@ -8,11 +8,11 @@ const authRouter = require('./auth');
 const validateRouter = require('./validate');
 const externalAPIRouter = require('./externalAPI');
 
-module.exports = (models, controllers) => {
+module.exports = (models, controllers, middlewares) => {
   router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   router.use('/user', userRouter(models));
   router.use('/post', postRouter(models, controllers.post));
-  router.use('/auth', authRouter(models, controllers.auth));
+  router.use('/auth', authRouter(models, controllers.auth, middlewares));
   router.use('/validate', validateRouter(models, null));
   router.use('/externalAPI', externalAPIRouter());
   return router

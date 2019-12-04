70858aaa530143b1a09f1f3bc42f7f7e6d7c8208
Min-92 <0201.mj.lee@gmail.com>
2019-12-04 16:49:35 +0900

    feat : 일반함수 화살표 함수로 변경

--- app.js
+++ app.js
@@ -50,12 +50,12 @@ app.use(middlewares.v1.renewToken);
 app.use('/v1', apiRouters.v1);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

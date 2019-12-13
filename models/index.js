9f30d0a02ef1b70699bdff94eee2fb925cabd243
Jimin Yeon <gmini.y@gmail.com>
2019-12-13 16:24:44 +0900

    [refactor] 관계 설정

--- models/index.js
+++ models/index.js
@@ -14,17 +14,80 @@ db.Image = require('./image')(sequelize, Sequelize);
 db.Location = require('./location')(sequelize, Sequelize);
 db.follow = require('./follow')(sequelize, Sequelize);
 
-db.Post.hasMany(db.Image, {as: "images", foreignKey : 'post_id' , sourceKey : 'id', onDelete: 'cascade', hooks: true });
-db.Image.belongsTo(db.Post, {as: "post", foreignKey : 'post_id' , targetKey : 'id' });
+db.Post.hasMany(
+  db.Image,
+  {
+    as: "images",
+    foreignKey: 'post_id',
+    sourceKey: 'id',
+    onDelete: 'cascade',
+    hooks: true,
+  },
+);
 
-db.Location.hasMany(db.Post, {as: "posts", foreignKey : 'location_id' , sourceKey : 'id'});
-db.Post.belongsTo(db.Location, {as: "location", foreignKey : 'location_id' , targetKey : 'id'});
+db.Image.belongsTo(
+  db.Post,
+  {
+    as: "post",
+    foreignKey: 'post_id',
+    targetKey: 'id',
+  },
+);
 
-db.User.hasMany(db.Post, {foreignKey : {name: 'writer_id', allowNull: false}, sourceKey : 'id'});
-db.Post.belongsTo(db.User, {foreignKey : 'writer_id' , targetKey : 'id'});
+db.Location.hasMany(
+  db.Post,
+  {
+    as: "posts",
+    foreignKey: 'location_id',
+    sourceKey: 'id'
+  },
+);
 
-db.User.belongsToMany(db.User, {as : 'followers', foreignKey : 'following_id', through : 'follow'});
-db.User.belongsToMany(db.User, {as : 'followings', foreignKey : 'follower_id', through : 'follow'});
+db.Post.belongsTo(
+  db.Location,
+  {
+    as: "location",
+    foreignKey: 'location_id',
+    targetKey: 'id'
+  },
+);
+
+db.User.hasMany(
+  db.Post,
+  {
+    foreignKey : {
+      name: 'writer_id',
+      allowNull: false
+    },
+    sourceKey: 'id',
+  },
+);
+
+db.Post.belongsTo(
+  db.User,
+  {
+    foreignKey: 'writer_id',
+    targetKey: 'id',
+  },
+);
+
+db.User.belongsToMany(
+  db.User,
+  {
+    as: 'followers',
+    foreignKey: 'following_id',
+    through: 'follow',
+  },
+);
+
+db.User.belongsToMany(
+  db.User,
+  {
+    as: 'followings',
+    foreignKey: 'follower_id',
+    through: 'follow',
+  },
+);
 
 db.Post.addHook('afterDestroy', async post => {
   try {
